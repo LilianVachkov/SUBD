cb1fd8e5f2cdb33ce3e9c34790858c9be34e9883
LilianVachkov <lilvachkov@gmail.com>
2017-06-23 15:28:04 +0300

    Add files via upload

---
+++ VisualSQL/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace VisualSQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.connectIons = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.Status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.grd = new System.Windows.Forms.DataGridView();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grd)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(165, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SQL Code Editor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Database Select:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tables:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(244, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fields:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(362, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Stored Procedures:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(475, 58);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Views procedures:";
+            // 
+            // connectIons
+            // 
+            this.connectIons.FormattingEnabled = true;
+            this.connectIons.Location = new System.Drawing.Point(12, 74);
+            this.connectIons.Name = "connectIons";
+            this.connectIons.Size = new System.Drawing.Size(86, 147);
+            this.connectIons.TabIndex = 6;
+            this.connectIons.SelectedIndexChanged += new System.EventHandler(this.connectIons_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(127, 74);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(86, 147);
+            this.listBox1.TabIndex = 7;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(247, 74);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(86, 147);
+            this.listBox2.TabIndex = 8;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(365, 74);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(86, 147);
+            this.listBox3.TabIndex = 9;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(478, 74);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(86, 147);
+            this.listBox4.TabIndex = 10;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 477);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(582, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Status
+            // 
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(0, 17);
+            this.Status.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(10, 260);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(554, 60);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 227);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Query:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(365, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 27);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Execute Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(478, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 27);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // grd
+            // 
+            this.grd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grd.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grd.Location = new System.Drawing.Point(0, 326);
+            this.grd.Name = "grd";
+            this.grd.Size = new System.Drawing.Size(582, 151);
+            this.grd.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 499);
+            this.Controls.Add(this.grd);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.connectIons);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grd)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ToolStripStatusLabel Status;
+        public System.Windows.Forms.StatusStrip statusStrip1;
+        public System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.ListBox listBox2;
+        public System.Windows.Forms.ListBox listBox3;
+        public System.Windows.Forms.ListBox listBox4;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label label5;
+        public System.Windows.Forms.Label label6;
+        public System.Windows.Forms.ListBox connectIons;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView grd;
+
+    }
+}
+
---
+++ VisualSQL/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+
+namespace VisualSQL
+{
+    public partial class Form1 : Form
+    {
+
+        private static string hiddenString;
+
+
+        public void getDBinfo(string constring, string qry, ListBox lst)
+        {
+            // get the db info
+            SqlConnection myConn = new SqlConnection(constring);
+            SqlDataReader reader;
+
+            SqlCommand cmd = new SqlCommand(qry, myConn);
+            cmd.CommandType = CommandType.Text;
+
+            myConn.Open();
+            try
+            {
+                Status.Text = string.Empty;
+                reader = cmd.ExecuteReader();
+                lst.DataSource = reader;
+            }
+            catch (Exception ex)
+            {
+                Status.Text = ex.ToString();
+            }
+            finally
+            {
+                if (myConn != null)
+                    myConn.Dispose();
+            }
+        }
+
+        protected void connectIons_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string db = connectIons.SelectedItem.ToString();
+            string constring = ConfigurationManager.ConnectionStrings[db].ToString();
+            string qry = "";
+            string table = "%";
+            hiddenString = constring;
+
+            qry = "SELECT Name from Sysobjects where xtype = 'u'";
+            getDBinfo(constring, qry, listBox1);
+
+            qry = "select c.name as Name from sys.columns c inner join sys.tables t on t.object_id = c.object_id and t.name like '" + table + "' and t.type = 'U'";
+            getDBinfo(constring, qry, listBox2);
+
+            qry = "select name from sys.procedures";
+            getDBinfo(constring, qry, listBox3);
+
+            qry = "select name from sys.views";
+            getDBinfo(constring, qry, listBox4);
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            foreach (ConnectionStringSettings c in System.Configuration.ConfigurationManager.ConnectionStrings)
+            {
+                connectIons.Items.Add(c.Name);
+            }
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string constring = hiddenString.ToString();
+
+            string qry = "select c.name as Name from sys.columns c inner join sys.tables t on t.object_id = c.object_id and t.name like '" + connectIons.SelectedItem.ToString() + "' and t.type = 'U'";
+            getDBinfo(constring, qry, listBox2);
+        }
+
+        protected void getSchemaInfo(string constring, string qry, RichTextBox txt)
+        {
+
+            // used for retrieving the db schema info rendering it in a label
+            constring = hiddenString.ToString();
+
+            SqlConnection myConn = new SqlConnection(constring);
+            SqlDataReader reader;
+
+            SqlCommand cmd = new SqlCommand(qry, myConn);
+            cmd.CommandType = CommandType.Text;
+
+            myConn.Open();
+            try
+            {
+                Status.Text = string.Empty;
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    txt.Text = reader.GetString(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                Status.Text = ex.ToString();
+            }
+            finally
+            {
+
+                if (myConn != null)
+                    myConn.Dispose();
+
+                if (cmd != null)
+                    cmd.Dispose();
+            }
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+ 
+            string constring = hiddenString.ToString();
+
+            string qry = "select ROUTINE_DEFINITION from INFORMATION_SCHEMA.ROUTINES Where ROUTINE_NAME='" + listBox2.SelectedItem.ToString() + "'";
+            getSchemaInfo(constring, qry, richTextBox1);
+        }
+
+        protected void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string constring = hiddenString.ToString();
+
+            string qry = "select VIEW_DEFINITION from INFORMATION_SCHEMA.VIEWS Where TABLE_NAME='" + listBox3.SelectedItem.ToString() + "'";
+            getSchemaInfo(constring, qry, richTextBox1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constring = hiddenString.ToString();
+ 
+            SqlConnection myConn = new SqlConnection(constring);
+            SqlDataReader reader;
+            string qry = richTextBox1.ToString();
+ 
+            SqlCommand cmd = new SqlCommand(qry, myConn);
+            cmd.CommandType = CommandType.Text;
+ 
+            myConn.Open();
+            try
+            {
+                Status.Text = string.Empty;
+                reader = cmd.ExecuteReader();
+                grd.DataSource = reader;
+                
+            }
+            catch (Exception ex)
+            {
+                Status.Text = ex.ToString();
+                //throw;
+            }
+            finally
+            {
+ 
+                if (myConn != null)
+                    myConn.Dispose();
+ 
+                if (cmd != null)
+                    cmd.Dispose();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {           
+                try
+                {
+                    StreamWriter sw = new StreamWriter("output.csv", false);
+                    int iColCount = grd.Columns.Count;
+                    for (int i = 0; i < iColCount; i++)
+                    {
+                        sw.Write(grd.Columns[i]);
+                        if (i < iColCount - 1)
+                        {
+                            sw.Write(",");
+                        }
+                    }
+                    sw.Write(sw.NewLine);
+
+                    foreach (DataRow dr in grd.Rows)
+                    {
+                        for (int i = 0; i < iColCount; i++)
+                        {
+                            if (!Convert.IsDBNull(dr[i]))
+                            {
+                                sw.Write(dr[i].ToString());
+                            }
+                            if (i < iColCount - 1)
+                            {
+                                sw.Write(",");
+                            }
+                        }
+
+                        sw.Write(sw.NewLine);
+                    }
+                    sw.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+        }
+}
+
